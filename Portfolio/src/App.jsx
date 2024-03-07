2028d452b194a4a3be590a1885e719a75de2e045
Eknoor007 <eknoorsingh011@gmail.com>
2024-03-07 16:06:55 +0530

    07|03|2024 04:06PM

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Dashboard from './Pages/Dashboard'
+import NavBar from './Pages/NavBar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Index from './Pages/Index'
+import Contact from './Pages/Contact'
+import MyProjects from './Pages/MyProjects'
+import Blog from './Pages/Blog'
 
 
 export default function App() {
   return (
-    <Dashboard/>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/MyProjects" element={<MyProjects />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
